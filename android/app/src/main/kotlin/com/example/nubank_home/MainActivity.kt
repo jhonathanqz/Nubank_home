a49f16fb9e85d26e1872609eb03090da28c39da4
jhonathanqz029 <74057391+jhonathanqz029@users.noreply.github.com>
2021-06-22 16:39:28 -0300

    Design nubank's old home

---
+++ android/app/src/main/kotlin/com/example/nubank_home/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.nubank_home
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:nubank_home/pages/splash/splash_page.dart';
+
+void main() {
+  SystemChrome.setSystemUIOverlayStyle(
+    SystemUiOverlayStyle(
+      statusBarBrightness: Brightness.dark,
+      statusBarColor: Colors.transparent,
+      systemNavigationBarColor: Colors.purple[800],
+    ),
+  );
+  runApp(
+    MyApp(),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Nubank Home Design',
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+        brightness: Brightness.dark,
+      ),
+      home: SplashPage(),
+    );
+  }
+}
